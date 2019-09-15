e4e3db91d66e3116a09acb8bf47b02f473a0909f
durgakiran <gatemefuture@gmail.com>
2019-09-15 11:31:59 +0530

    completed

--- models.js
+++ models.js
@@ -1,27 +1,144 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const schema  = mongoose.Schema;
+const schema = mongoose.Schema;
 
 let User = new schema({
-  "username": {"type": String, "required": true, "unique": true, maxlength: [20, 'username too long']}
+  "username": {
+    "type": String,
+    "required": true,
+    "unique": true,
+    maxlength: [20, 'username too long']
+  }
 });
 
 let userModel = mongoose.model('User', User);
 
 
-var createAndSaveUser = function(data, done){
+let Exercise = new schema({
+  "userId": {
+    "type": String,
+    "required": true
+  },
+  "description": {
+    "type": String,
+    "required": true
+  },
+  "duration": {
+    "type": Number,
+    min: 1,
+    "required": true
+  },
+  "date": {
+    "type": Date,
+    default: Date.now
+  }
+})
+
+
+// validate userId, and add "username" to the exercise instance
+Exercise.pre('save', function(next) {
+  mongoose.model('User').findById(this.userId, (err, user) => {
+    if(err) return next(err)
+    if(!user) {
+      const err = new Error('unknown userId')
+      err.status = 400
+      return next(err)
+    }
+    this.username = user.username
+    if(!this.date) {
+      this.date = Date.now()
+    }
+    next();
+  })
+})
+
+let exerciseModel = mongoose.model('Exercise', Exercise);
+
+
+var createAndSaveUser = function (data, done) {
   console.log(data);
   let user = new userModel(data);
   console.log(user);
-  user.save(function (err, data) {
+  //done(null, user);
+  user.save((err, data) => {
+    if (err) done(err, null);
     console.log('save success!');
-    done(null , data);
+    done(null, data);
   });
 }
 
+
+//add exercise
+var createExercise = function (data, done) {
+  console.log(data);
+  let exercise = new exerciseModel(data);
+  console.log(exercise);
+  //done(null, user);
+  exercise.save((err, data) => {
+    if (err) done(err, null);
+    console.log('save success!');
+    userModel.find({"_id": data.userId}, (err, user) => {
+      console.log(user);
+      if(err) done(err, user);
+      exerciseModel.find({"userId": data.userId}, (err, exercises) => {
+        let tmpData = {};
+        console.log(exercises);
+        tmpData.user = user[0];
+        tmpData.exercises = exercises;
+        console.log(tmpData);
+        done(null, [tmpData]);
+      })
+    })
+  });
+}
+
+
+var getAllUsers = function (done) {
+  userModel.find({}, (err, data) => {
+    console.log(data);
+    if (err) done(err, null);
+    done(null, data);
+  })
+}
+
+var getExercisesByUserId = function (userId, done) {
+  userModel.find({"_id": userId}, (err, user) => {
+    console.log(user);
+    if(err) done(err, null);
+    exerciseModel.find({"userId": userId}, (err, exercises) => {
+      let tmpData = {};
+      console.log(exercises);
+      tmpData.user = user[0];
+      tmpData.exercises = exercises;
+      console.log(tmpData);
+      done(null, [tmpData]);
+    })
+  })
+}
+
+var getExercisesByUserIdFromTo = function (userId,fromDate, toDate, done) {
+  userModel.find({"_id": userId}, (err, user) => {
+    console.log(user);
+    if(err) done(err, null);
+    exerciseModel.find({"userId": userId, "date": { $gt: new Date(fromDate), $lt: new Date(toDate)}}, (err, exercises) => {
+      let tmpData = {};
+      console.log(exercises);
+      tmpData.user = user[0];
+      tmpData.exercises = exercises;
+      console.log(tmpData);
+      done(null, [tmpData]);
+    })
+  })
+}
+
+
+
 /* all exports*/
 
-exports.userModel = userModel ;
+exports.userModel = userModel;
 exports.createAndSaveUser = createAndSaveUser;
-
+exports.createExercise = createExercise;
+exports.getAllUsers = getAllUsers;
+exports.getExercisesByUserId = getExercisesByUserId;
+exports.getExercisesByUserIdFromTo = getExercisesByUserIdFromTo;
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require('cors')
 var router = express.Router();
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect("mongodb://durgakiran:vijay17791@ds157187.mlab.com:57187/exercise-tracker" || 'mongodb://localhost/exercise-track', { useMongoClient: true/* other options */ } )
+mongoose.connection.on('connected', function () {
+	console.log('Mongoose default connection open to ');
+});
+
 
 app.use(cors())
 
@@ -36,8 +40,57 @@ router.post('/new-user', (req, res, next) => {
     //console.log('daas' + JSON.stringify(data));
     res.json(data);
   })
-  next();
+  //next();
 });
+
+var createExercise = require('./models.js').createExercise;
+router.post('/add', (req, res, next) => {
+  createExercise(req.body, function(err, data) {
+    if(err) {
+      //console.log("error happened" + err);
+      return (next(err)); 
+    }
+    if(!data) {
+      console.log('Missing `done()` argument');
+      return next({message: 'Missing callback argument'});
+    }
+    //console.log('daas' + JSON.stringify(data));
+    res.json(data);
+  })
+});
+
+var getAllUsers = require('./models.js').getAllUsers;
+router.get('/users', (req, res, next) => {
+  getAllUsers((err, data) => {
+    if(err) res.send("something bad happened");
+    else res.json(data);
+  })
+})
+
+var getExercisesByUserId = require('./models.js').getExercisesByUserId;
+var getExercisesByUserIdFromTo = require('./models.js').getExercisesByUserIdFromTo;
+router.get('/log', (req, res, next) => {
+  let userId = req.query.userId;
+  let fromDate = null;
+  let toDate = null;
+  if(req.query.from && req.query.to) {
+    fromDate = req.query.from;
+    toDate = req.query.to;
+  }
+  console.log(fromDate)
+  if (fromDate) {
+    getExercisesByUserIdFromTo(userId,fromDate, toDate,  (err, data) => {
+      if(err) next(err);
+      else res.json(data);
+    })
+  }else {
+    getExercisesByUserId(userId, (err, data) => {
+      if(err) next(err);
+      else res.json(data);
+    })
+  }
+})
+
 
 
 // Error Handling middleware
